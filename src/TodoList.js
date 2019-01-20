33bcbd35e35ea3f777e07b8d0f76e474b01781c7
wdrik <w.drik@outlook.com>
2019-01-20 17:29:23 -0200

    basic redux structure

---
+++ src/TodoList.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as TodoActions from './store/actions/todos';
+
+const TodoList = ({ todos, addTodo, removeTodo }) => (
+  <Fragment>
+    <ul>
+      { todos.map(todo => 
+        <li key={todo.id}>
+          {todo.text} 
+          <button onClick={() => removeTodo(todo.id)}>X</button>
+        </li>
+      ) }
+    </ul>
+    <button onClick={() => addTodo('teste')}>Novo Todo</button>
+  </Fragment>
+);
+
+TodoList.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    text: PropTypes.string,
+  })).isRequired
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(TodoActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
