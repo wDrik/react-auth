096c55b4003157a9815a017be3f135ede0399f31
wdrik <w.drik@outlook.com>
2019-01-20 18:06:04 -0200

    app new structure!

---
+++ src/pages/main/index.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+
+class Main extends Component {
+  render() {
+    return(
+      <Fragment>
+        <form>
+
+        </form>
+      </Fragment>
+    );
+  }
+}
+
+export default Main;
