2685d8582d6f9d547b141a7c5645ee64dcbabfad
wdrik <w.drik@outlook.com>
2019-01-20 23:39:32 -0200

    Footer

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,15 +1,72 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as FavoriteActions from '../../store/actions/favorites';
 
 class Main extends Component {
+  static propTypes = {
+    addFavorite: PropTypes.func.isRequired,
+    favorites: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      url: PropTypes.string,
+    })).isRequired,
+  }
+
+  state = {
+    repositoryInput: '',
+  }
+
+  handleAddRepository = e => {
+    e.preventDefault();
+
+    this.props.addFavorite();
+  }
+
   render() {
+    console.log(this.props);
+
     return(
       <Fragment>
-        <form>
-
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
         </form>
+
+        <ul>
+          {this.props.favorites.map(favorite => (
+            <li key={favorite.id}>
+              <p>
+                <strong>{favorite.name}</strong>
+                ({favorite.description}).
+              </p>
+
+              <a
+                href={favorite.url}
+                target="_blank"
+                rel="noopener noreferrer">
+                Acessar
+              </a>
+            </li>
+          ))}
+        </ul>
       </Fragment>
     );
   }
 }
 
-export default Main;
+const mapStateToProps = state => ({
+  favorites: state.favorites
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
